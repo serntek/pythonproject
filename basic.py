e7e6cdb06da92c5c0b01888f7760a4ef304c427f
Taylor Brosseau <serntek@gmail.com>
2017-11-23 01:06:03 -0500

    first python ever woohoo

---
+++ basic.py
@@ -0,0 +1,26 @@
+
+# this is a single line comment
+''' this is a multiple
+line
+comment '''
+
+
+""" this is a multiline 
+
+comment """
+'''Double quote string'''
+print("hello world")
+'''single quote string '''
+print('hello world')
+
+
+'''STAY CONSITENT
+ WITH YOUR QUOTES'''
+
+'''Use triple quotes to make strings with commas and quotes work!'''
+
+print('''lorem fdsfsdf,df;dfs'dsfsdfsf'sdfdsfsd'fsdfsf'sdfsdfs'dfd'f'sfs,dfsfsf;sdffsdfsf''')
+
+'''Square brackets at the end with a number inside will only print that amount of letters or numbers'''
+
+print('Hello'[0])
