1de8783f3cb7c46383f381ad06bf429f456d1922
Taylor Brosseau <serntek@gmail.com>
2017-11-23 04:40:51 -0500

    No longer stuck ...

---
+++ condtionals.py
@@ -0,0 +1,24 @@
+'''CONDITIONALS'''
+
+'''variable with the integer 4'''
+x = 7
+
+
+'''basic if'''
+if x < 6:
+    print('this is true! 4 is less than 6')
+
+
+'''basic if else'''
+
+if x < 6:
+    print('this is true')
+else:
+
+    print('this is false')
+
+    '''elif'''
+
+    color = 'red'
+    if color == 'red':
+     print('color is red')
