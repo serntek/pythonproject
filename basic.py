6f380a2f7ddf9c429f2d0536062cfc9017b9f9fd
Taylor Brosseau <serntek@gmail.com>
2017-11-23 01:27:51 -0500

    Getting the hang of this

--- basic.py
+++ basic.py
@@ -21,6 +21,30 @@ print('hello world')
 
 print('''lorem fdsfsdf,df;dfs'dsfsdfsf'sdfdsfsd'fsdfsf'sdfsdfs'dfd'f'sfs,dfsfsf;sdffsdfsf''')
 
-'''Square brackets at the end with a number inside will only print that amount of letters or numbers'''
+'''Square brackets at the end of stringwith a number inside will only print that amount of letters or numbers'''
 
 print('Hello'[0])
+print('Hello'[2])
+
+''' sub strings/ to print more than one letter but not all'''
+print('Hello'[1:3])
+
+
+''' number, "integer" no quotes needed '''
+print(212)
+
+''' same line, with spaces because of comma!'''
+print(1,2,3,'hello')
+
+
+'''separate line! make sure to add an n after the backslash'''
+
+print('line1\nline2\nline3')
+
+'''keep punctuation'''
+print('C:\\\somehwere\\user')
+
+'''or'''
+
+print(r'C:\\\somehwere\\user')
+
---
+++ variables.py
@@ -0,0 +1,39 @@
+'''Variables and data types'''
+
+greeting = 'hello world'
+
+greeting = 'hello everyone'#Reassisn, not append
+
+print(greeting)
+
+
+'''data types'''
+'''string'''
+myString = "hello"
+'''integer'''
+myInteger = 25
+
+'''float'''
+myFloat = 25.34
+
+'''Python IS case sensitive'''
+print(myFloat)
+
+'''this is a list'''
+myList = [1,2,3,'hello']
+
+print(myList)
+
+'''This is a dictionary'''
+myDict = {'a':1,'b':2,'c':3}
+
+'''functions'''
+
+print(type(myString),myString)
+print(type(myInteger),myInteger)
+print(type(myFloat),myFloat)
+print(type(myDict),myDict)
+print(type(myList),myList)
+
+print(myList[3])
+print(myDict['a'])
